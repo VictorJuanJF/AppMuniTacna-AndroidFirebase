1de52f3733c07b65a80b750c133fe4b2c19039fc
VictorJuanJF <vj.jimenez96@gmail.com>
2018-08-31 23:21:11 -0500

    Maps added

--- app/src/main/java/com/example/jimenez/appmunitacna/MandarReporteActivity.java
+++ app/src/main/java/com/example/jimenez/appmunitacna/MandarReporteActivity.java
@@ -1,19 +1,46 @@
 package com.example.jimenez.appmunitacna;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MandarReporteActivity extends AppCompatActivity {
 
+    private static final int CAMERA_REQUEST_CODE = 1000;
+
+
+    Categoria mCategoria;
     @BindView(R.id.imgFoto)
     AppCompatImageView imgFoto;
     @BindView(R.id.toolbar)
@@ -22,16 +49,19 @@ public class MandarReporteActivity extends AppCompatActivity {
     CollapsingToolbarLayout toolbarLayout;
     @BindView(R.id.app_bar)
     AppBarLayout appBar;
-    @BindView(R.id.etNombre)
-    TextInputEditText etNombre;
-    @BindView(R.id.etApellidos)
-    TextInputEditText etApellidos;
-    @BindView(R.id.etDescripcion)
-    TextInputEditText etDescripcion;
-
-    Categoria mCategoria;
     @BindView(R.id.tvTitulo)
     TextView tvTitulo;
+    @BindView(R.id.etTituloReporte)
+    TextInputEditText etTituloReporte;
+    @BindView(R.id.etUbicacion)
+    TextInputEditText etUbicacion;
+    @BindView(R.id.etDescripcion)
+    TextInputEditText etDescripcion;
+    @BindView(R.id.btnEnviar)
+    Button btnEnviar;
+    @BindView(R.id.btnUbicacion)
+    Button btnUbicacion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +71,40 @@ public class MandarReporteActivity extends AppCompatActivity {
         configArtista();
         configActionBar();
         configImageView();
+        takePhoto();
+        getUbicacion();
 
 
+    }
+
+    private void getUbicacion() {
+        int permissionsCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionsCheck == PackageManager.PERMISSION_DENIED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                //Show an explanation to the user
+
+            } else {
+                //No explanation needed, we can request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            }
+
+        }
+
+
+
+    }
+
+    private void takePhoto() {
+        imgFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
     }
 
     private void configImageView() {
@@ -60,5 +122,82 @@ public class MandarReporteActivity extends AppCompatActivity {
         mCategoria = MainActivity.sCategoria;
         tvTitulo.setText(mCategoria.getNombre());
 
+    }
+
+    private void openCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
+
+            imgFoto.setImageBitmap(bitmap);
+
+        }
+    }
+
+    @OnClick(R.id.btnUbicacion)
+    public void onUbicacionClicked() {
+        Log.d("ubicacion", "Hola!");
+        // Location Listener
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+// Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                if (location.getLatitude() != 0.0 && location.getLongitude() != 0.0) {
+                    try {
+                        Geocoder geocoder = new Geocoder(MandarReporteActivity.this, Locale.getDefault());
+                        List<Address> list = geocoder.getFromLocation(
+                                location.getLatitude(), location.getLongitude(), 1);
+                        if (!list.isEmpty()) {
+                            Address DirCalle = list.get(0);
+                            etUbicacion.setText(DirCalle.getAddressLine(0));
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                switch (status) {
+                    case LocationProvider.AVAILABLE:
+                        Log.d("debug", "LocationProvider.AVAILABLE");
+                        break;
+                    case LocationProvider.OUT_OF_SERVICE:
+                        Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                        break;
+                    case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                        Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                        break;
+                }
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+                // Este metodo se ejecuta cuando el GPS es desactivado
+                etUbicacion.setText("GPS Desactivado");
+            }
+
+        };
+
+// Register the listener with the Location Manager to receive location updates
+        int permissionsCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
     }
 }
